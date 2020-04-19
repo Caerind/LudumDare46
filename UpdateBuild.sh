0a7fd5cc6a49180e45b7fc05ec89ec147b4c397a
Cmdu76 <charles.mailly@free.fr>
2020-04-19 14:57:00 +0200

    Auto share build

---
+++ UpdateBuild.sh
@@ -0,0 +1,7 @@
+cp build/LudumDare46-Client/Debug/LudumDare46Client.exe ../Demo/LD46.exe
+rm -rf ../Demo/Assets
+cp -r Assets ../Demo/Assets
+
+cp build/LudumDare46-Client/Debug/LudumDare46Client.exe C:/Users/charl/Dropbox/LD46/Demi/LD46.exe
+rm -rf C:/Users/charl/Dropbox/LD46/Demi/Assets
+cp -r Assets C:/Users/charl/Dropbox/LD46/Demi/Assets
