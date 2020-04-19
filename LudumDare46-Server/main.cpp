677b02e53cce78f26f88078f67681830bc057d0d
Cmdu76 <charles.mailly@free.fr>
2020-04-19 22:28:29 +0200

    Up

--- LudumDare46-Server/main.cpp
+++ LudumDare46-Server/main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char** argv)
 	if (argc >= 1)
 	{
 		en::PathManager::GetInstance().SetExecutablePath(argv[0]);
+		printf("FontPath: %s + %s", argv[0], en::PathManager::GetInstance().GetFontsPath().c_str());
 	}
-	printf("FontPath: %s + %s", argv[0], en::PathManager::GetInstance().GetFontsPath().c_str());
 
 	en::LogManager::GetInstance().Initialize();
 
